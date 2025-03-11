30dcf486e3667be46d597904bca4502de2203c6a
Joseph <j.mburu.pro@gmail.com>
2025-03-11 13:22:19 +0300

    drivers

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution here
+function findMatching(drivers, name) {
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+  }
+  
+  function fuzzyMatch(drivers, letters) {
+    return drivers.filter(driver => driver.startsWith(letters));
+  }
+  
+  function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name === name);
+  }
+  
+  const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
+  const datastructure = [
+    { name: 'Bobby', hometown: 'Pittsburgh' },
+    { name: 'Sammy', hometown: 'New York' },
+    { name: 'Sally', hometown: 'Cleveland' },
+    { name: 'Annette', hometown: 'Los Angeles' },
+    { name: 'Bobby', hometown: 'Tampa Bay' }
+  ];
+  
+  console.log(findMatching(drivers, 'Bobby'));
+  console.log(fuzzyMatch(drivers, 'Sa'));
+  console.log(matchName(datastructure, 'Bobby'));
+  
+  
